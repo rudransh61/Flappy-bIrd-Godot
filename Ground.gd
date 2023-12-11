4f8020f3117ccdc7abb9c139b7cf90fc4cfc495b
Rudransh Bhardwaj <aarti19830@gmail.com>
2023-12-11 18:26:48 +0530

    idk some error

---
+++ Ground.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+signal hit
+
+
+func _on_body_entered(body):
+	pass#hit.emit() # Replace with function body.
--- Main.gd
+++ Main.gd
@@ -1,11 +1,12 @@
 extends Node
 
+@export var pipe_scene : PackedScene
 
 var game_running : bool
 var game_over : bool
 var scroll
 var score
-const SCROLL_SPEED : int = 4
+const SCROLL_SPEED : float = 0.00004
 var screen_size : Vector2
 var ground_height : int
 var pipes : Array
@@ -14,6 +15,8 @@ const PIPE_RANGE : int = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screen_size = get_viewport().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game() # Replace with function body.
 
 func new_game():
@@ -40,3 +43,12 @@ func _input(event):
 func start_game():
 	game_running = true
 	$Bird.flying = true
+
+
+func _process(delta):
+	if game_running:
+		scroll += SCROLL_SPEED
+		
+		if scroll >= screen_size.x :
+			scroll = 0
+		$Ground.position.x -= scroll
